5fb3fc377a055920982e489859fa0ac585c641fc
miaoyu <miaoyu2009@qq.com>
2021-05-11 18:22:57 +0800

    feat: storybook

--- src/components/TitleText/index.tsx
+++ src/components/TitleText/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { View } from "@tarojs/components";
+// import { View } from "@tarojs/components";
+import { View } from "@tarojs/components/dist-h5/react";
+
+
 
 interface ITitleTextProps {}
 
