a120419b4a2216c3cf57dd2cac44190d3f348229
miaoyu <miaoyu2009@qq.com>
2021-05-21 11:54:28 +0800

    doc: titleText description

--- src/stories/TitleText.stories.tsx
+++ src/stories/TitleText.stories.tsx
@@ -3,19 +3,27 @@ import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 // import { actions } from "@storybook/addon-actions";
 
-import TitleText, {ITitleTextProps} from "../components/TitleText";
+import TitleText, { ITitleTextProps } from "../components/TitleText";
 
 export default {
   title: "Example/TitleText",
   component: TitleText,
+  parameters: {
+    docs: {
+      description: {
+        component: `
+标题文本，可自定义字体颜色、大小等
+        `,
+      },
+    },
+  },
   argTypes: {
-    color: { control: 'color' },
+    color: { control: "color" },
   },
 } as Meta;
 
 const Template: Story<ITitleTextProps> = (args) => <TitleText {...args} />;
 
-export const Basic = Template.bind({})
+export const Basic = Template.bind({});
 
-Basic.args = TitleText.defaultProps 
-
+Basic.args = TitleText.defaultProps;
