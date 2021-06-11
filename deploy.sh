aed0c9329b829fda5ab94df06fc0cdfc141dd700
miaoyu <miaoyu2009@qq.com>
2021-06-11 14:59:25 +0800

    chore: modify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,20 @@
 git checkout -b deploy &&
+echo "=======> git checkout -b deploy"
+git checkout -b deploy &&
+	echo "=======> yarn build-storybook"
 yarn build-storybook &&
-git add storybook-static && 
+	echo "=======> add storybook-static"
+git add storybook-static &&
+	echo "=======> git commit"
 git commit -m "Initial storybook-static subtree commit" &&
+	echo "=======> git remote set-url origin git@github.com:goldEli/lwi-mobile-ui.git"
 git remote set-url origin git@github.com:goldEli/lwi-mobile-ui.git &&
-git subtree push --prefix storybook-static origin gh-pages &&
+	echo "=======> subtree push"
+# git subtree push --prefix storybook-static origin gh-pages &&
+git push origin `git subtree split --prefix storybook-static develop`:gh-pages --force &&
+	echo "=======> checkout develop"
 git checkout develop &&
-git branch -d deploy &&
+	echo "=======> branch -d deploy"
+git branch -D deploy &&
+	echo "=======> remote set-url origin git@gitee.com:liweijia/lwj-mobile-ui.git"
 git remote set-url origin git@gitee.com:liweijia/lwj-mobile-ui.git
